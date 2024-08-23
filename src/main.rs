6f704dfea070b62154d14a112a70a67e347d8a51
cdreamer1 <cdreamer267@gmail.com>
2024-08-23 01:08:09 -0600

    refactor: reivse the main function using enum

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,27 @@
+enum Message {
+    Quit,
+    Move { x: i32, y: i32 },
+    Write(String),
+    ChangeColor(i32, i32, i32)
+}
+
+// struct QuitMessage;
+// struct MoveMessage {
+//     x: i32,
+//     y: i32,
+// }
+// struct WriteMessage(String);
+// struct ChangeColorMessage(i32, i32, i32);
+
+impl Message {
+    fn call(&self) {
+
+    }
+}
+
 fn main() {
     println!("Hello, world!");
+
+    let m = Message::Write(String::from("Hello, World!"));
+    m.call();
 }
