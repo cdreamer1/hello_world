34e6cd9839f9d6e10df9659bad55c852f49394f3
cdreamer1 <cdreamer267@gmail.com>
2024-08-26 02:23:59 -0600

    feat: add some parts in regard to vector

--- src/main.rs
+++ src/main.rs
@@ -24,4 +24,16 @@ fn main() {
 
     let m = Message::Write(String::from("Hello, World!"));
     m.call();
+
+    let mut v = vec![1, 2, 3, 4, 5];
+
+    let third: &i32 = &v[2];
+    println!("The third element is {third}");
+
+    let third: Option<&i32> = v.get(2);
+    match third {
+        Some(third) => println!("The third element is {third}"),
+        None => println!("There is no third element"),
+    }
+
 }
